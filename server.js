5ea24a865999f0144ab498d7f8b15ee8a40f2859
James Logue <logueja@gmail.com>
2018-03-06 13:40:02 -0700

    Add public assetes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (request, response) => {
-  response.send('hello world')
+app.use(express.static('public'))
+
+const urlLogger = (request, response, next) => {
+  console.log('Request URL:', request.url)
+  next()
+}
+
+app.get('/json', urlLogger, (request, response) => {
+  response.status(200).json({"name": "James"})
 })
 
 app.listen(3000, () => {
   console.log('express intro running on localhost:3000')
 })
+
